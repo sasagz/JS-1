9b81547647e7ae5831d5be833579c8b97634b1d8
sasagz_escola <163011955+sasagz@users.noreply.github.com>
2024-09-17 11:59:31 -0300

    .

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelector('.mudarTextoBotao').addEventListener('click',function(){
+    const paragrafo = document.querySelector ('.descricao');
+    paragrafo.textContent = 'texto alterado!';
+});
+
+
+document.querySelector('.destacarBotao').addEventListener('click',function(){
+    const titulo = document.querySelector ('.titulo');
+    titulo.classList.add = ('destaque');
+});
+
